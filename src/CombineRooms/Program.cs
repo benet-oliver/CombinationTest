506641b36c69b4a864f75cc793f4ad5f462ef056
Benet Oliver Noguera <benet.oliver@gmail.com>
2021-03-09 20:11:12 +0100

    add test in program

--- src/CombineRooms/Program.cs
+++ src/CombineRooms/Program.cs
@@ -1,44 +1,81 @@
 ﻿using CombineRooms.CombineHelpers;
 using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
+using System.Linq;
 
 namespace CombinationRooms
 {
     public static class Program
     {
-        private const int numberOfRooms = 3;
+        private static readonly List<int> _numberOfRoomsList = new List<int>() { 1, 2, 3, 4, 5, 6, 7 };
+
         public static void Main(string[] args)
         {
-            //using StreamReader sr = new StreamReader("response.json");
-            //var providerResponse = JsonConvert.DeserializeObject<ProviderResponse>(sr.ReadToEnd());
+            foreach(var room in _numberOfRoomsList)
+            {
+                GenerateCombinations(room);
+            }
+        }
 
-            //var roomDedupeCombination1 = RoomDedupeCombinationFactory.GetRoomDedupeCombination<Rate>(ApiVersion.V1, numberOfRooms);
+        private static void GenerateCombinations(int numberOfRooms)
+        {
+            Stopwatch sw = new Stopwatch();
 
-            //var roomDedupeCombination2 = RoomDedupeCombinationFactory.GetRoomDedupeCombination<Rate>(ApiVersion.V2, numberOfRooms);
+            Console.WriteLine($"Starting combination for {numberOfRooms} rooms");
 
-            //var roomDedupeCombination3 = RoomDedupeCombinationFactory.GetRoomCombination<Rate>();
+            List<IRoomDedupeCombination<Rate>> roomDedupeList = new List<IRoomDedupeCombination<Rate>>();
+            roomDedupeList.Add(new RoomDedupeCombinationRoomType<Rate>());
+            roomDedupeList.Add(new RoomDedupeCombinationAll<Rate>());
+            roomDedupeList.Add(new RoomCombinationAll<Rate>());
+            roomDedupeList.Add(new RoomCombinationHotelbeds());
+            //roomDedupeList.Add(new RoomCombinationHotelbedsAlternative());
 
-            //foreach (var hotel in providerResponse.hotels.hotels)
-            //{
-            //    foreach (var room in hotel.rooms)
-            //    {
-            //        foreach (var rate in room.rates)
-            //        {
-            //            for (int i = 1; i <= numberOfRooms; i++)
-            //            {
-            //                var roomData = new RoomData<Rate>(hotel.code.ToString(), rate.boardCode, i, room.code, rate, rate.net, new RoomHash(true));
+            AddRoomsToRoomDedupeCombinationList(GetFakeResponse(), roomDedupeList, numberOfRooms);
 
-            //                roomDedupeCombination1.AddRoom(roomData);
-            //                roomDedupeCombination2.AddRoom(roomData);
-            //                roomDedupeCombination3.AddRoom(roomData);
-            //            }
-            //        }
-            //    }
-            //}
+            foreach (var roomDedupe in roomDedupeList)
+            {
+                sw.Restart();
+                PrintOptions(roomDedupe, roomDedupe.GetDedupedRoomCombinations().Count(), sw.ElapsedMilliseconds);
+            }
 
-            //var combinations1 = roomDedupeCombination1.GetDedupedRoomCombinations();
-            //var combinations2 = roomDedupeCombination2.GetDedupedRoomCombinations();
-            //var combinations3 = roomDedupeCombination3.GetDedupedRoomCombinations();
+            Console.WriteLine($"Finish combination for {numberOfRooms} rooms");
+
+        }
+
+        private static ProviderResponse GetFakeResponse()
+        {
+            using StreamReader sr = new StreamReader("Provider/response.json");
+            return JsonConvert.DeserializeObject<ProviderResponse>(sr.ReadToEnd());
+        }
+
+        private static void AddRoomsToRoomDedupeCombinationList(
+            ProviderResponse providerResponse,
+            List<IRoomDedupeCombination<Rate>> roomDedupeList,
+            int numberOfRooms)
+        {
+            foreach (var (hotel, room, rate) in from hotel in providerResponse.hotels.hotels
+                                                from room in hotel.rooms
+                                                from rate in room.rates
+                                                select (hotel, room, rate))
+            {
+                for (int i = 1; i <= numberOfRooms; i++)
+                {
+                    var roomData = new RoomData<Rate>(hotel.code.ToString(), rate.boardCode, i, room.code, rate, rate.net, new RoomHash(true));
+
+                    foreach (var roomDedupe in roomDedupeList)
+                    {
+                        roomDedupe.AddRoom(roomData);
+                    }
+                }
+            }
+        }
+
+        private static void PrintOptions(IRoomDedupeCombination<Rate> roomDedupeCombination, int count, long elapsedMs)
+        {
+            Console.WriteLine($"Implementation {roomDedupeCombination.GetType().Name} return {count} options, elapsed {elapsedMs} ms");
         }
     }
 }
